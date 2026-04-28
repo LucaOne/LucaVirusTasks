0837b218f44dd9f786e4b0f9c89f3d18224e76df
YongHe <sanyuan.hy@alibaba-inc.com>
2026-04-28 18:58:48 +0800

    feature: update code

--- src/utils.py
+++ src/utils.py
@@ -903,11 +903,8 @@ def calc_emb_filename_by_seq_id(seq_id, embedding_type):
     if seq_id[0] == ">":
         seq_id = seq_id[1:]
     if "|" in seq_id:
-        strs = seq_id.split("|")
-        if len(strs) > 1:
-            emb_filename = embedding_type + "_" + strs[1].strip() + ".pt"
-        else:
-            emb_filename = embedding_type + "_" + seq_id.replace(" ", "").replace("/", "_") + ".pt"
+        print(f"Warning: the seq id({seq_id}) contain invalid char '|', it will be replace with '_'.")
+        emb_filename = embedding_type + "_" + seq_id.replace(" ", "").replace("/", "_").replace("|", "_") + ".pt"
     else:
         emb_filename = embedding_type + "_" + seq_id.replace(" ", "").replace("/", "_") + ".pt"
     return emb_filename
